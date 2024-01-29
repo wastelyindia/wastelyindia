87beb314a0c6f5474622ab85ee1a5ceae4c42364
wastelyindia <158072263+wastelyindia@users.noreply.github.com>
2024-01-29 21:16:48 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
