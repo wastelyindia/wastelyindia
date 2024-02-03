c3022e9a4e1d5e0789fd10d7e344921de3de0a4d
wastelyindia <158072263+wastelyindia@users.noreply.github.com>
2024-02-03 01:22:57 +0000

    about company comments

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -125,7 +125,8 @@ const About = () => {
         <CardContainer />
       </div>
 
-      <div className="container">
+{/* Partner company */}
+      {/* <div className="container">
         <div className="mx-auto max-w-2xl py-12 px-6 lg:max-w-7xl lg:py-32 lg:px-8">
           <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
             <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
@@ -145,7 +146,7 @@ const About = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
 
       <div className="container">
